04b1175cfe07bccd91af2dec34c6bb52490a4948
jona-tech <jonathanbernhardsson1@gmail.com>
2022-11-08 01:14:50 +0000

    Add question and answers array to seperate .js file named questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,201 @@
+const questions = [{
+    question: 'How many tones exist in the chromatic scale?',
+    answers: [{
+            text: '8',
+            correct: false
+        },
+        {
+            text: '9',
+            correct: false
+        },
+        {
+            text: '14',
+            correct: false
+        },
+        {
+            text: '12',
+            correct: true
+        }
+    ]
+},
+{
+    question: 'Who wrote the song "Blinded By The Light"?',
+    answers: [{
+            text: 'Bob Dylan',
+            correct: false
+        },
+        {
+            text: 'Bob Marley',
+            correct: false
+        },
+        {
+            text: 'Bruce Springsteen',
+            correct: true
+        },
+        {
+            text: 'Manfred Mann',
+            correct: false
+        }
+    ]
+},
+{
+    question: 'Where does the "Nyckelharpa" come from?',
+    answers: [{
+            text: 'Sweden',
+            correct: true
+        },
+        {
+            text: 'Norway',
+            correct: false
+        },
+        {
+            text: 'Denmark',
+            correct: false
+        },
+        {
+            text: 'Finland',
+            correct: false
+        }
+    ]
+},
+{
+    question: 'Where does the "Hardingfela" come from?',
+    answers: [{
+            text: 'Sweden',
+            correct: false
+        },
+        {
+            text: 'Norway',
+            correct: true
+        },
+        {
+            text: 'Denmark',
+            correct: false
+        },
+        {
+            text: 'Finland',
+            correct: false
+        }
+    ]
+},
+{
+    question: 'What is the average radio output frequency',
+    answers: [{
+            text: '432 Hz',
+            correct: false
+        },
+        {
+            text: '528 Hz',
+            correct: false
+        },
+        {
+            text: '420 Hz',
+            correct: false
+        },
+        {
+            text: '440 Hz',
+            correct: true
+        }
+    ]
+},
+{
+    question: 'What is a "Stradivarius"?',
+    answers: [{
+            text: 'Violin',
+            correct: true
+        },
+        {
+            text: 'Piano',
+            correct: false
+        },
+        {
+            text: 'Program',
+            correct: false
+        },
+        {
+            text: 'Guitar',
+            correct: false
+        }
+    ]
+},
+{
+    question: 'Who shot the sheriff?',
+    answers: [{
+            text: 'I shot the sheriff',
+            correct: true
+        },
+        {
+            text: 'You shot the sheriff',
+            correct: false
+        },
+        {
+            text: 'He shot the sheriff',
+            correct: false
+        },
+        {
+            text: 'He shot himself',
+            correct: false
+        }
+    ]
+},
+{
+    question: 'What is the standard tuning of a guitar?',
+    answers: [{
+            text: 'DADGAD',
+            correct: false
+        },
+        {
+            text: 'CDEFGA',
+            correct: false
+        },
+        {
+            text: 'EADGBE',
+            correct: true
+        },
+        {
+            text: 'GEADBE',
+            correct: false
+        }
+    ]
+},
+{
+    question: 'Which of these is a DAW',
+    answers: [{
+            text: 'Nuendo',
+            correct: true
+        },
+        {
+            text: 'Cubase',
+            correct: true
+        },
+        {
+            text: 'Ableton',
+            correct: true
+        },
+        {
+            text: 'Logic',
+            correct: true
+        }
+    ]
+},
+{
+    question: 'What instrument did Niccolo Paganini play?',
+    answers: [{
+            text: 'Drums',
+            correct: false
+        },
+        {
+            text: 'Piano',
+            correct: false
+        },
+        {
+            text: 'Violin',
+            correct: true
+        },
+        {
+            text: 'Guitar',
+            correct: false
+        }
+    ]
+}
+]
