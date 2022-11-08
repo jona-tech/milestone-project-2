df0e2e37a38388c4b8aa795cec141d3a0b5013d6
jona-tech <jonathanbernhardsson1@gmail.com>
2022-11-08 01:12:15 +0000

    Add empty functions to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,35 @@ const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
+
+function incrementScore() {
+
+}
+
+function startQuiz() {
+
+}
+
+function setNextQuestion() {
+
+}
+
+function showQuestion() {
+
+}
+
+function resetState() {
+
+}
+
+function selectAnswer() {
+
+}
+
+function setStatusClass() {
+
+}
+
+function clearStatusClass() {
+    
+}
