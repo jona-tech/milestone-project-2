e2605825d6d21514748d1065914d0cfafc15ebdd
jona-tech <jonathanbernhardsson1@gmail.com>
2022-11-08 01:08:44 +0000

    Declare variables to script file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/**Basic code structure inspired by Web Dev Simplified on Youtube (https://www.youtube.com/watch?v=riDzcEQbX6k)  with a few personal changes */
+
+/** Variables */
+
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
